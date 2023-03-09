011c871cde72300468658d530970cb8608a98032
Paola Ferrari <paola220041@ilum.cnpem.br>
2023-03-09 17:06:53 -0300

    atualizaçao da aula 1

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,3 +1,28 @@
+def gene_cb():
+    """ Gera um gene válido para o problema das caixas binárias
+    
+    Return:
+        Um valos de zeros ou um
+    """
+    lista = [0, 1]
+    gene = random.choice(lista)
+    return gene
+
+def individuo_cb (n):
+    ''' Gera um individuo para o problema da caixa binaria
+    
+    Args:
+       n: numero de genes do individuo
+    
+    Return:
+        Uma lista com n genes. Cada gene é um valor zero ou um
+    '''
+    individuo = []
+    for i in range(n):
+        gene = gene_cb()
+        individuo.append(gene)
+    return individuo
+
 def funcao_objetivo_cb(individuo):
     ''' Computa o problema objetivo no problema das caixas binárias
     
