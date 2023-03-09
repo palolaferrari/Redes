b1e8bfa8ed1a94e54d37ea16afe22a957333f2c0
Paola Ferrari <paola220041@ilum.cnpem.br>
2023-03-09 16:45:03 -0300

    notebooks das aulas

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,13 @@
+def funcao_objetivo_cb(individuo):
+    ''' Computa o problema objetivo no problema das caixas binárias
+    
+    Args:
+        individuo: lista contendo os genes das caixas binárias
+    
+    Return:
+        Um valor representando uma soma dos genes do indivíduo
+    '''
+    return sum(individuo)
+
+
  
