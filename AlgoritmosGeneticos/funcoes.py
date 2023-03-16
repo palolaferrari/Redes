368e0f05e4cc4200e49a9bf6da2a5ab49b867340
Paola Ferrari <paola220041@ilum.cnpem.br>
2023-03-16 16:42:42 -0300

    atualizacao funcoes.py e experimento 3

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,3 +1,5 @@
+import random
+
 def gene_cb():
     """ Gera um gene válido para o problema das caixas binárias
     
@@ -7,6 +9,7 @@ def gene_cb():
     lista = [0, 1]
     gene = random.choice(lista)
     return gene
+
 
 def individuo_cb (n):
     ''' Gera um individuo para o problema da caixa binaria
@@ -23,6 +26,39 @@ def individuo_cb (n):
         individuo.append(gene)
     return individuo
 
+
+def populacao_cb(tamanho, n):
+    ''' cria uma população no problema das caixas binárias.
+    
+    Args:
+       n: numero de gene de um individuo
+       tamanho: tamanho da população
+    
+    Return:
+        Uma lista onde cada item é um individuo. Um individuo é uma lista com n genes.
+    '''
+    populacao = []
+    for _ in range(tamanho):
+        populacao.append(individuo_cb(n)) #ele está criando uma pop a partir de uma funcao que ja existe, n vezes
+    return populacao
+
+
+def selecao_roleta_max (populacao, fitness): #depende dos individuos que vão ser sorteados, o quão bom o individuo é dentro da função
+    '''seleciona individuos de uma populacao usando o método de roleta. 
+    
+    Nota: apenas funciona para problemas de maximizaçao.
+    
+    Args:
+       populacao: lista com todos os individuos da populacao
+       fitness: valor da função objetivo dos individuos da população
+       
+    Returns:
+       populações dos individuos selecionados.
+    '''
+    populacao_selecionada = random.choices(populacao, weights = fitness, k = len(populacao)) 
+    return populacao_selecionada 
+
+
 def funcao_objetivo_cb(individuo):
     ''' Computa o problema objetivo no problema das caixas binárias
     
@@ -34,5 +70,22 @@ def funcao_objetivo_cb(individuo):
     '''
     return sum(individuo)
 
+
+def funcao_objetivo_pop_cb(populacao):
+    ''' calcula a funcao objetivo para todos os membros de uma populacao
+    
+    Args: 
+       populacao: lista com todos os individuos da populacao.
+    
+    Return:
+       lista de valores representando a fitness de cada individuo
+    '''
+    fitness = []
+    for individuo in populacao:
+        fobj = funcao_objetivo_cb(individuo)
+        fitness.append(fobj)
+    return fitness
+        
+    
 
  
