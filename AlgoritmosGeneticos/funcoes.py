d82d9666ed92a549ebb62dd1812442b7c08564ba
Paola Ferrari <paola220041@ilum.cnpem.br>
2023-03-23 17:00:25 -0300

    atualizacao aula 23/03/23

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,5 +1,9 @@
 import random
 
+#################################################################
+#                           genes
+#################################################################
+
 def gene_cb():
     """ Gera um gene válido para o problema das caixas binárias
     
@@ -10,6 +14,25 @@ def gene_cb():
     gene = random.choice(lista)
     return gene
 
+
+def gene_cnb(valor_max_caixa):
+    ''' gera um gene válido para o problema das caixas não binárias.
+    
+    Args: 
+         valor_max_caixa: valor máximo que a caixa pode assumir
+    Return: 
+         Um valor de 0 a 'valor_max_caixa' (incluso - vamos incluir o último valor)
+    '''
+    gene = random.randint(0, valor_max_caixa)
+    return gene
+
+
+def gene_letra():
+    '''
+    '''
+#################################################################
+#                        indivíduo
+#################################################################
 
 def individuo_cb (n):
     ''' Gera um individuo para o problema da caixa binaria
@@ -27,6 +50,27 @@ def individuo_cb (n):
     return individuo
 
 
+def individuo_cnb(numero_genes, valor_max_caixa):
+    ''' Gera um individuo para o problema da caixa não binária
+    
+    Args:
+       numero_genes: número de genes do individuo (lista)
+       valor_max_caixa: valor máximo que a caixa pode assumir
+
+    Return:
+        Uma lista que representa um indivpiduo válido para o problema das CNB
+    '''
+    individuo = []
+    for _ in range (numero_genes):
+        gene= gene_cnb(valor_max_caixa)
+        individuo.append(gene)
+    return individuo
+    
+
+#################################################################
+#                        população
+#################################################################
+
 def populacao_cb(tamanho, n):
     ''' cria uma população no problema das caixas binárias.
     
@@ -42,6 +86,29 @@ def populacao_cb(tamanho, n):
         populacao.append(individuo_cb(n)) #ele está criando uma pop a partir de uma funcao que ja existe, n vezes
     return populacao
 
+
+def populacao_cnb(tamanho_populacao, numero_genes,valor_max_caixa):
+    ''' cria uma população no problema das caixas binárias
+    
+    Args:
+        tamanho_populacao: numero de individuos da populacao
+        numero_genes: numero de genes do individuo
+        valor_max_caixa: valor maximo que a caixa pode assumir
+        
+    Return:
+        uma lista onde cada item representa um individuo
+    '''
+    populacao = []
+    for _ in range(tamanho_populacao):
+        individuo = individuo_cnb(numero_genes, valor_max_caixa)
+        populacao.append(individuo) #ele está criando uma pop a partir de uma funcao que ja existe, n vezes
+    return populacao
+    
+
+
+#################################################################
+#                        seleção
+#################################################################
 
 def selecao_roleta_max (populacao, fitness): #depende dos individuos que vão ser sorteados, o quão bom o individuo é dentro da função
     '''seleciona individuos de uma populacao usando o método de roleta. 
@@ -59,8 +126,12 @@ def selecao_roleta_max (populacao, fitness): #depende dos individuos que vão se
     return populacao_selecionada 
 
 
-    
 
+
+#################################################################
+#                       cruzamento
+#################################################################
+    
 def cruzamento_ponto_simples(mae, pai):
     ''' Operador de cruzamento de ponto simples.
     
@@ -77,8 +148,12 @@ def cruzamento_ponto_simples(mae, pai):
     filho2 = mae [:ponto_de_corte] + pai [ponto_de_corte:]
     
     return filho1, filho2
-    
-    
+
+
+
+#################################################################
+#                         mutação 
+#################################################################
 
 def mutacao_cb(individuo):
     '''realiza a mutacao de um gene no problema das caixas binárias.
@@ -92,8 +167,28 @@ def mutacao_cb(individuo):
     gene_a_ser_mutado = random.randint(0, len(individuo)-1)
     individuo[gene_a_ser_mutado] = gene_cb()
     return individuo
+
+
+def mutacao_cnb(individuo, valor_max_caixa):
+    '''realiza a mutacao de um gene no problema das caixas nao binárias.
     
+    Args:
+      individuo: uma lista representando um individuo no problema das caixas nao binárias.
+      valor_max_caixa: valor maximo que a caixa pode assumir
+      
+    Return:
+       Um individuo com gene mutado
+    '''    
+    gene_a_ser_mutado = random.randint(0, len(individuo)-1)
+    individuo[gene_a_ser_mutado] = gene_cnb(valor_max_caixa)
+    return individuo
     
+
+    
+
+#################################################################
+#                 Função objetivo - indivíduos
+#################################################################
 
 def funcao_objetivo_cb(individuo):
     ''' Computa o problema objetivo no problema das caixas binárias
@@ -106,6 +201,21 @@ def funcao_objetivo_cb(individuo):
     '''
     return sum(individuo) + 1 # +1 para nao cair no erro de tudo zero
 
+def funcao_objetivo_cnb(individuo):
+    ''' calcula o fitness para o problema das caixas nao binarias
+    
+    Args:
+        individuo: lista que representa um individuo dentro do problema das cnb
+        
+    Return:
+        Um valor que representa a fitness do individuo
+    '''
+    fitness = sum(individuo)
+    return fitness
+
+#################################################################
+#                 Função objetivo - população              
+#################################################################
 
 def funcao_objetivo_pop_cb(populacao):
     ''' calcula a funcao objetivo para todos os membros de uma populacao
@@ -123,6 +233,20 @@ def funcao_objetivo_pop_cb(populacao):
     return fitness
         
     
-
+def funcao_objetivo_pop_cnb(populacao):
+    ''' calcula a funcao objetivo para a populacao completa
+    
+    Args: 
+       populacao: lista com todos os individuos da populacao.
+    
+    Return:
+       lista de valores representando a fitness de cada individuo
+    '''
+    fitness_pop = []
+    for individuo in populacao:
+        fitness_ind = funcao_objetivo_cnb(individuo)
+        fitness_pop.append(fitness_ind)
+    return fitness_pop
+        
 
  
